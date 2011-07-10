2e1aada67863a878c013dde71dd4d59fd62b7849
Jose Rodrigo Perez <joepr25@gmail.com>
2011-07-10 12:36:20 -0500

    Se anadio la funcionalidad para mandar email pero me manda un error, creo es por las credenciales de hotmail, voy a tratar de resolverlo y volverlo a subir.

--- application/controllers/controladortweets.php
+++ application/controllers/controladortweets.php
@@ -27,6 +27,47 @@ class Controladortweets extends Ci_Controller{
         
         function loademail() {
             
-            $this->load->view('emailview');
+            $data['Entro']=0;
+            $this->load->view('emailview', $data);
+        }
+        
+        function sendemail() {
+            
+            $this->load->library('form_validation');
+            $this->load->library('email');
+            
+            $this->form_validation->set_rules('destinatario', 'Correo Destinatario', 'trim|required|valid_email');
+            $this->form_validation->set_rules('asunto', 'Asunto', 'trim|required');
+            $this->form_validation->set_rules('mensaje', 'Mensaje', 'trim|required');
+            
+            if($this->form_validation->run() == FALSE)
+            {
+                $data['Entro']=1;
+                $this->load->view('emailview', $data);
+            }
+            else
+            {
+                        $asunto = $_POST["asunto"];
+			$email = $_POST["destinatario"];
+                        $mensaje = $_POST["mensaje"];
+			
+			$this->email->set_newline("\r\n");
+
+			$this->email->from('ntalk_pruebas@hotmail.com', 'Joe Pruebas');
+			$this->email->to($email);		
+			$this->email->subject($asunto);		
+			$this->email->message($mensaje);
+
+
+			if($this->email->send())
+			{
+                            $this->load->view('firstpage');
+			}
+
+			else
+			{
+				show_error($this->email->print_debugger());
+			}
+            }
         }
 }
--- application/views/emailview.php
+++ application/views/emailview.php
@@ -1,17 +1,24 @@
 <html>
     <head>
         <title>Prueba de Mandar un email a Gmail</title>
+        <link rel="stylesheet" href="http://localhost/Prueba1/CSS/style.css" type="text/css" media="screen" charset="utf-8" />
     </head>
     <body>
-        <form action="./sendemail" method="post">
-            Destinatario(Correo Gmail):   <input type="text" name="destinatario" />
-            <br>
-            <h3>Asunto:                       </h3><input type="text" name="asunto" />
-            <br>
-            Mensaje:                      <input type="text" name="mensaje" />
-            <br>
-            <input type="submit" />
-        </form>
-        
+        <div align="center" id="forma">
+            <form action="./sendemail" method="post">
+                Destinatario:   <input type="text" name="destinatario"  id="destinatario" />
+                <br>
+                Asunto:                       <input type="text" name="asunto" id="asunto" />
+                <br>
+                Mensaje:                      <input type="text" name="mensaje" id="mensaje" />
+                <br>
+                <input type="submit" />
+                <?php
+                    if($Entro == 1)
+                        echo validation_errors();
+                ?>
+                
+            </form>
+        </div>
     </body>
 </html>
